2768dbdab06150c6538ae2424409ccc4cb2a943b
Adam Strohm <adam@omeka2.ads.gl.iit.edu>
2015-06-05 14:27:12 -0500

    upgrade to omeka 2.3

--- plugins/ExhibitBuilder/views/shared/exhibit_layouts/gallery/layout.php
+++ plugins/ExhibitBuilder/views/shared/exhibit_layouts/gallery/layout.php
@@ -6,16 +6,22 @@ $showcaseFile = $showcasePosition !== 'none' && !empty($attachments);
 $galleryPosition = isset($options['gallery-position'])
     ? html_escape($options['gallery-position'])
     : 'left';
+$galleryFileSize = isset($options['gallery-file-size'])
+    ? html_escape($options['gallery-file-size'])
+    : 'square_thumbnail';
+$captionPosition = isset($options['captions-position'])
+    ? html_escape($options['captions-position'])
+    : 'center';
 ?>
 <?php if ($showcaseFile): ?>
-<div class="gallery-showcase <?php echo $showcasePosition; ?> with-<?php echo $galleryPosition; ?>">
+<div class="gallery-showcase <?php echo $showcasePosition; ?> with-<?php echo $galleryPosition; ?> captions-<?php echo $captionPosition; ?>">
     <?php
         $attachment = array_shift($attachments);
         echo $this->exhibitAttachment($attachment, array('imageSize' => 'fullsize'));
     ?>
 </div>
 <?php endif; ?>
-<div class="gallery <?php if ($showcaseFile || !empty($text)) echo "with-showcase $galleryPosition"; ?>">
-    <?php echo $this->exhibitAttachmentGallery($attachments); ?>
+<div class="gallery <?php if ($showcaseFile || !empty($text)) echo "with-showcase $galleryPosition"; ?> captions-<?php echo $captionPosition; ?>">
+    <?php echo $this->exhibitAttachmentGallery($attachments, array('imageSize' => $galleryFileSize)); ?>
 </div>
 <?php echo $text; ?>
